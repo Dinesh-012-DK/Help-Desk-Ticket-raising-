0328b21cbc2fa2d9469c7e2cc72fa8a4b4b19a6a
Dinesh-012-DK <dineshkumarmani012@gmail.com>
2025-08-13 15:35:55 -0700

    Allowed Host

--- Ticket_raising/Ticket_raising/settings.py
+++ Ticket_raising/Ticket_raising/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-#(2u88-1dfjdq3r#q)9)d*7rup@o)rj1ut2%wsnv7!j8yota^&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
